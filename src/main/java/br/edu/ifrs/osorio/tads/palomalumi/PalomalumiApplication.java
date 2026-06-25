7a70d525db49dea51a91054681020264f113c3b3
PalomaLumi <2024020183@aluno.osorio.ifrs.edu.br>
2026-06-25 19:37:47 -0300

    Extend PalomalumiApplication with SpringBootServletInitializer
    Extend PalomalumiApplication to support servlet initialization.

--- src/main/java/br/edu/ifrs/osorio/tads/palomalumi/PalomalumiApplication.java
+++ src/main/java/br/edu/ifrs/osorio/tads/palomalumi/PalomalumiApplication.java
@@ -2,12 +2,17 @@ package br.edu.ifrs.osorio.tads.palomalumi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PalomalumiApplication {
+public class PalomalumiApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PalomalumiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PalomalumiApplication.class, args);
+    }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(PalomalumiApplication.class);
+    }
 }
